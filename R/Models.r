f68783b2cd77a0e5745014446463f78031b5b40c
Jennifer McIntyre <55552826+SheepwormJM@users.noreply.github.com>
2026-09-04 16:41:29 +0100

    Add comments on GLMs and effect size reporting
    Add comments regarding effect size and confidence intervals.

---
+++ R/Models.r
@@ -0,0 +1,3 @@
+# It is good practice to report both the effect size and an estimate of it's confidence intervals
+
+# Dan Haydon's pdf book on GLMs: There are various R commands that are useful for calculating summaries of fitted values like ggeffects(model, ‘term’)in the package ggpredict, or emmeans(model ~ term) in the package emmeans).
